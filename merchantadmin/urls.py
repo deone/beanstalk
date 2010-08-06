e0e332cbb4998612bda436dae4e88a9aa21f22b5
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-08-06 13:44:20 +0100

    Removed merchantadmin app. Django admin is flexible and customizable to achieve what we need.

--- merchantadmin/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('merchantadmin.views',
-	(r'^$', 'index'),
-)
--- merchantadmin/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def index(request, template="merchantadmin/index.html"):
-
-    return render_to_response(template, {
-    }, context_instance=RequestContext(request))
