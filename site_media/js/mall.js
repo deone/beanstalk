3ae2c434ded3060b26d86718f9eff33c876b68b6
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-08-10 17:18:26 +0100

    Disable "Add To Cart" onclick. pay4memall 0.1r3

--- site_media/js/mall.js
+++ site_media/js/mall.js
@@ -1,4 +1,7 @@
-function addToCart()    {
+function addToCart()	{
+
+    $("#add_to_cart_btn").attr({disabled: "disabled", value: "Please Wait..."});
+
     var productId = $("#add_to_cart #product_id").val();
     var qty = $("#add_to_cart #id_quantity").val();
 
@@ -6,7 +9,8 @@ function addToCart()    {
 
     $.post(url, { quantity: qty }, 
 	function(response)  {
-	    if (response.data.type != "boolean")	{
+	    $("#add_to_cart_btn").attr({disabled: "", value: "Add To Cart"});
+	    if (response.data.type != "boolean")    {
 		alert("Failed data type check");
 	    } else	{
 		displayShoppingCart();
