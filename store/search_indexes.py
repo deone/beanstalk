953ab544587531c92e53a3c75bca73fd0235b0a2
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-08-19 13:39:28 +0100

    Search settings and templates for store app.

---
+++ store/search_indexes.py
@@ -0,0 +1,8 @@
+from haystack.indexes import *
+from haystack import site
+from store.models import Product
+
+class ProductIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+    
+site.register(Product, ProductIndex)
