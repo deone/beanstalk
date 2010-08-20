3d6d8a421167c78f77b8b9247e30686a374b38d7
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-08-20 12:00:45 +0100

    Order change form customization in admin. * We need to customize add/change product_detail, add/change product, add/change product_group.

--- order/admin.py
+++ order/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django import forms
 from order.models import *
 
+class OrderedItemInline(admin.TabularInline):
+    model = OrderedItem
+    extra = 0
+    readonly_fields = ("buyer", "product", "cost", "quantity",)
+
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields = ("store", "order_id", "amount", "status", "created_at", "date_paid", "validation_number")
+    readonly_fields = ("order_id", "amount", "status",)
     list_display = ("order_id", "amount", "status", "date_paid")
     date_hierarchy = "created_at"
+    fieldsets = (
+	    (None, {
+		'fields': (("order_id", "amount", "status"),)
+	    }),
+    )
+
+    inlines = [
+	OrderedItemInline,
+    ]
     
     def queryset(self, request):
 	qs = super(OrderAdmin, self).queryset(request)
@@ -12,15 +27,5 @@ class OrderAdmin(admin.ModelAdmin):
 	    return qs
 	return qs.filter(store__owner=request.user)
 
-class OrderedItemAdmin(admin.ModelAdmin):
-    readonly_fields = ("order", "buyer", "product", "quantity", "cost")
-    list_display = ("order", "product", "quantity", "cost")
-
-    def queryset(self, request):
-	qs = super(OrderedItemAdmin, self).queryset(request)
-	if request.user.is_superuser:
-	    return qs
-	return qs.filter(order__store__owner = request.user)
-
 admin.site.register(Order, OrderAdmin)
-admin.site.register(OrderedItem, OrderedItemAdmin)
+admin.site.register(OrderedItem)
