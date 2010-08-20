d0d1d9ad3d1fff3302f9dbae2e751d6201b39a1f
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-08-20 15:01:41 +0100

    Let's use RealTimeSearchIndex for now, we don't have high traffic yet.

--- store/search_indexes.py
+++ store/search_indexes.py
@@ -2,7 +2,7 @@ from haystack.indexes import *
 from haystack import site
 from store.models import Product
 
-class ProductIndex(SearchIndex):
+class ProductIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
     
 site.register(Product, ProductIndex)
