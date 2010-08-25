c886683eebb7c74c4ebbb434e9f81f3a73363692
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-08-25 15:16:29 +0100

    Pass base_forms in account views.

--- account/views.py
+++ account/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login as auth_login
 
-import all_forms as af
 from account.forms import *
+from all_forms import base_forms
+
+CONTEXT = base_forms
 
 def index(request, template="account/index.html"):
     """ Account home, shows the user's account settings. """
@@ -29,12 +31,11 @@ def register(request, template="account/register.html", form_class=RegisterForm)
 	    return redirect(redirect_to)
     else:
 	form = form_class()
+
+    context = CONTEXT
+    context.update({"redirect_to": redirect_to, "register_form": form})
 	
-    return render_to_response(template, {
-		"redirect_to": redirect_to,
-		"register_form": form,
-		"form_set": af.mall_forms,
-	    }, context_instance=RequestContext(request))
+    return render_to_response(template, context, context_instance=RequestContext(request))
 
 def login(request, template="account/login.html", form_class=LoginForm):
     try:
@@ -52,11 +53,10 @@ def login(request, template="account/login.html", form_class=LoginForm):
     else:
 	form = form_class()
 
-    return render_to_response(template, {
-		"redirect_to": redirect_to,
-		"login_form": form,
-		"form_set": af.mall_forms,
-	    }, context_instance=RequestContext(request))
+    context = CONTEXT
+    context.update({"redirect_to": redirect_to, "login_form": form})
+
+    return render_to_response(template, context, context_instance=RequestContext(request))
 
 def logout(request, template="mall/index.html"):
 
--- all_forms.py
+++ all_forms.py
@@ -2,7 +2,7 @@ from mall.forms import *
 from haystack.forms import SearchForm
 
 base_forms = {
-    "mall_search_form": SearchForm,
-    "store_select_form": StoreSelectForm,
-    "department_select_form": DepartmentSelectForm,
+    "mall_search_form": SearchForm(),
+    "store_select_form": StoreSelectForm(),
+    "department_select_form": DepartmentSelectForm(),
 }
