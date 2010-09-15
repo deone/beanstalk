b5162d17ac4cd100fb20187cbfefcad447077181
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-09-15 16:48:06 +0100

    Removed all_forms.

--- all_forms.py
+++
@@ -1,8 +0,0 @@
-from mall.forms import *
-from haystack.forms import SearchForm
-
-base_forms = {
-    "mall_search_form": SearchForm(),
-    "store_select_form": StoreSelectForm(),
-    "department_select_form": DepartmentSelectForm(),
-}
