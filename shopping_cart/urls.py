4f4a76a1b3f4ac56de71542fb44ab8d977a40d10
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-10-18 14:08:17 +0100

    Remove TEMPLATE_CONTEXT_PROCESSORS setting. Remove route to get_cart_details, /cart/details. Use h.get_global_context_variables() in store/views instead of get_common_context().

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -4,5 +4,4 @@ urlpatterns = patterns('shopping_cart.views',
     url(r'^$', 'preview_cart', name='preview_cart'),
     url(r'^update/(?P<product_id>\d+)/$', 'update_cart', name='update_cart'),
     url(r'^delete/(?P<product_id>\d+)/$', 'delete_from_cart', name='delete_from_cart'),
-    (r'^details/$', 'show_cart_details'),
 )
--- store/views.py
+++ store/views.py
@@ -6,61 +6,58 @@ from mall.models import Department
 from store.forms import StoreSorterForm
 from shopping_cart.forms import ShoppingCartForm
 
-from haystack.forms import SearchForm
 import helpers as h
 
 def get_store(store_slug):
     return get_object_or_404(Store, slug__iexact=store_slug)
 
-def get_common_context(store):
-    return {
-	"store": store,
-	"store_list": h.get_stores,
-	"department_list": h.get_departments,
-	"mall_search_form": SearchForm,
-    }
-
 def products_in_store(request, store_name):
+    context = h.get_global_context_variables(request.session._session)
     store = get_store(store_name)
+    context.update({"store": store})
 
     return list_detail.object_list(
 	    request,
 	    queryset = Product.objects.filter(product_group__store=store).order_by("-date_added"),
 	    template_name = "store/index.html",
-	    extra_context = get_common_context(store)
+	    extra_context = context,
     )
 
 def product_detail(request, store_name, product_id, form_class=ShoppingCartForm):
-    store = get_store(store_name)
-    context = get_common_context(store)
     product = Product.objects.get(pk=product_id)
 
     if request.method == "POST":
 	form = form_class(request.POST)
 	if form.is_valid():
-	    try:
-		# Check if product has been previously added to cart
-		product_demanded = request.session.get(product_id)
-	    except KeyError:
+	    product_quantity_in_cart = request.session.get(product_id)
+	    if product_quantity_in_cart is None:
 		quantity = int(request.POST["quantity"])
 		if not product.is_quantity_available(quantity):
 		    form = form_class()
-		    context.update({"feedback": "Insufficient Stock"})
+		    feedback = "Insufficient Stock."
 		else:
-		    form.add_item(request, product_id, float(product.price))
-		    context.update({"feedback": "Item added to cart."})
+		    form.add_item(request, product_id)
+		    feedback = "Item added to cart."
 	    else:
-		quantity = product_demanded[0] + int(request.POST["quantity"])
+		quantity = product_quantity_in_cart + int(request.POST["quantity"])
 		if not product.is_quantity_available(quantity):
 		    form = form_class()
-		    context.update({"feedback": "Insufficient Stock"})
+		    feedback = "Insufficient Stock"
 		else:
-		    form.add_to_item(request, product_id, product_demanded)
-		    context.update({"feedback": "Item updated in cart."})
+		    form.add_to_item(request, product_id, product_quantity_in_cart)
+		    feedback = "Item updated in cart."
+
     else:
 	form = form_class()
+	feedback = ""
 
-    context.update({"shopping_cart_form": form})
+    store = get_store(store_name)
+    context = h.get_global_context_variables(request.session._session)
+    context.update({
+	    "shopping_cart_form": form, 
+	    "store": store,
+	    "feedback": feedback,
+	})
 
     return list_detail.object_detail(
 	    request,
@@ -75,12 +72,13 @@ def products_in_store_product_group(request, store_name, product_group_id):
     store = get_store(store_name)
     product_group = get_object_or_404(ProductGroup, pk=product_group_id)
 
-    context = get_common_context(store)
+    context = h.get_global_context_variables(request.session._session)
     context.update({
 		"product_list_length": product_group.product_set.count(),
 		"product_group": product_group,
 		"store_sorter_form": StoreSorterForm,
-		})
+		"store": store,
+	    })
 
     return list_detail.object_list(
 	    request,
