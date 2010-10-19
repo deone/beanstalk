d3341c9aafaa2e3dc3f78defc55719cd94b86a2a
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-10-19 09:58:05 +0100

    Replace get_departments(), get_stores() and get_cart_from_session() with get_cart_details() and get_global_context_variables().

--- helpers.py
+++ helpers.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.utils.functional import Promise
 from django.utils.encoding import force_unicode
+from haystack.forms import SearchForm
 
 from mall.models import Department
-from store.models import Store
+from store.models import Store, Product
 
 from traceback import print_exc
 
@@ -53,16 +54,38 @@ def create_response(code, type=None, value=None):
 
     return simplejson.dumps(response, cls=LazyEncoder)
 
-def get_cart_from_session(session_object):
+def get_cart(session):
     cart = []
 	
-    for item in session_object.iteritems():
+    for item in session.iteritems():
 	if isinstance(item[0], unicode):
 	    cart.append(item)
+    
     return cart
 
-def get_departments():
-    return Department.objects.all()
+def get_cart_details(session):
+    if session.has_key("testcookie"):
+	session.delete_test_cookie()
 
-def get_stores():
-    return Store.objects.all()
+    cart = get_cart(session)
+	
+    items_qty = items_price = 0
+
+    for item in cart:
+	items_qty += item[1]
+	items_price += Product.objects.get(pk=item[0]).price * item[1]
+
+    cart_object = {
+	"quantity": items_qty,
+	"price": items_price,
+    }
+
+    return cart_object
+
+def get_global_context_variables(session):
+    return {
+	"department_list": Department.objects.all(),
+	"store_list": Store.objects.all(),
+	"mall_search_form": SearchForm(),
+	"cart": get_cart_details(session),
+    }
