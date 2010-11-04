7ea84ec2ce1fd92359e7e36579f87ae2d18220c5
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-11-04 10:00:27 +0100

    Include payment settings for live deployment in settings_live.py Inform user that an email has been sent to their email address.

--- settings_live.py
+++ settings_live.py
@@ -2,3 +2,8 @@ from settings import *
 
 DEBUG = TEMPLATE_DEBUG = False
 HAYSTACK_WHOOSH_PATH = "/home/pay4memall/pay4memall_index"
+
+MERCHANT_CODE = '4578956780'
+MERCHANT_KEY = '1984075237'
+MERCHANT_SERVICE_ID = '100'
+PAYMENT_URL = 'http://pay4me.demo.swgindia.com/order/payment/payprocess/v1/PID/' + MERCHANT_CODE
