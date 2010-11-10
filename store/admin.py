9e14430e9fb6f321057eb5fa6718284d0ed0fd09
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-11-10 14:07:12 +0100

    Set back owner field to select all users that can log into admin (for now).

--- store/admin.py
+++ store/admin.py
@@ -67,8 +67,8 @@ class StoreAdmin(admin.ModelAdmin):
 	if db_field.name == "owner":
 	    # When we start creating staff who are not superusers, we would need to rewrite this to use has_perms().
 	    kwargs["queryset"] = User.objects.filter(is_staff=True).exclude(is_superuser=True)
-	    if not request.user.is_superuser:
-		kwargs["queryset"] = User.objects.filter(pk=request.user.id)
+	    #if not request.user.is_superuser:
+		#kwargs["queryset"] = User.objects.filter(pk=request.user.id)
 	    return db_field.formfield(**kwargs)
 	return super(StoreAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
