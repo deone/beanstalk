6b2affeea741c90d5e3ae32f95f8233717233ad2
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-11-25 09:24:15 +0100

    Move buyer_order_confirmation_email.txt into buyer_order_confirmation_email.html. Quote variable names in send_notification docstring.

--- mail/__init__.py
+++ mail/__init__.py
@@ -4,7 +4,7 @@ from django.utils.html import strip_tags
 
 def send_notification(subject, sender, mail_template, *recipients, **context_vars):
     """
-	Send email notification to recipients using mail_template, replacing context variables with context_vars.
+	Send email notification to `recipients` using `mail_template`, replacing context variables with `context_vars`.
     """
     html_content = render_to_string(mail_template, context_vars)
     text_content = strip_tags(html_content)
