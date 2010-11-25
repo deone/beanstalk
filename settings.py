63e7b2e1bc27e4078bb356a6d64125d37629b9ce
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-11-25 09:47:38 +0100

    Enable CsrfViewMiddleware and MessageMiddleware in settings.py.

--- settings.py
+++ settings.py
@@ -71,9 +71,9 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'pay4memall.urls'
